7e060cdbadb331f8e19eed28a6632535fa216170
alenjoby <alenjoby955@gmail.com>
2024-04-04 16:20:48 +0400

    SoundBoard (Bonus)

---
+++ Chapter-5-Embedded Content/Exercises/Audio Sampler/script.js
@@ -0,0 +1,41 @@
+/*calling the specific functions*/
+function ahaha() {
+    var audio=document.getElementById(sn1)/*Accessing the specific audios by their "ID" */
+    sn1.play() /*Requesting the funtion to play the audio */
+}
+/*calling the specific functions*/
+function bang() {
+    var audio=document.getElementById(sn2)/*Accessing the specific audios by their "ID" */
+    sn2.play() /*Requesting the funtion to play the audio */
+}
+/*calling the specific functions*/
+function email() {
+    var audio=document.getElementById(sn3)/*Accessing the specific audios by their "ID" */
+    sn3.play() /*Requesting the funtion to play the audio */
+}
+/*calling the specific functions*/
+function dan() {
+    var audio=document.getElementById(sn4)/*Accessing the specific audios by their "ID" */
+    sn4.play() /*Requesting the funtion to play the audio */
+}
+/*calling the specific functions*/
+function confused() {
+    var audio=document.getElementById(sn5)/*Accessing the specific audios by their "ID" */
+    sn5.play() /*Requesting the funtion to play the audio */
+}
+/*calling the specific functions*/
+function iate() {
+    var audio=document.getElementById(sn6)/*Accessing the specific audios by their "ID" */
+    sn6.play() /*Requesting the funtion to play the audio */
+}
+/*calling the specific functions*/
+function hello() {
+    var audio=document.getElementById(sn7)/*Accessing the specific audios by their "ID" */
+    sn7.play() /*Requesting the funtion to play the audio */
+}
+/*calling the specific functions*/
+function back() {
+    var audio=document.getElementById(sn8)/*Accessing the specific audios by their "ID" */
+    sn8.play() /*Requesting the funtion to play the audio */
+}
+
